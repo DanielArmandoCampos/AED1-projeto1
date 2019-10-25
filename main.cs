0705109864c6e24dc1ff122b90b4f85390d87ca6
deividps <56140437+deividps@users.noreply.github.com>
2019-10-25 12:06:01 -0300

    Incluido função de banco de dados

--- main.cs
+++ main.cs
@@ -1,7 +1,8 @@
 using System;
 using System.IO;
 using System.Text;
-
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Acesso{
 
@@ -11,30 +12,11 @@ namespace Acesso{
 
       int Cadastrar;
 
+      List<string> contaAdocao = new List<string>();
+      List<string> contaDoacao = new List<string>();
+
       dataPets CadPets = new dataPets("ESPECIE","RAÇA","CIDADE","PORTE");
       dataUser CadUser = new dataUser("Nome", "Telefone", "cidade","Email");
-
-      string [,] DBUser = new string [1000,4];
-      
-      DBUser[0,0] = CadUser.Nome;
-      DBUser [0,1] = CadUser.Telefone;
-      DBUser[0,2] = CadUser.Localidade;
-      DBUser[0,3] = CadUser.Email;
-
-      string [,] DBPets = new string [1000,4];
-
-      for (int i = 0; i < 1000; i++)
-      {
-          for (int x = 0; x < 4; x++)
-          {
-              DBPets[i, x] = "";
-          }
-      }
-      DBPets[0,0] = CadPets.Especie;
-      DBPets [0,1] = CadPets.Raca;
-      DBPets[0,2] = CadPets.Localidade;
-      DBPets[0,3] = CadPets.Porte;
-
 
 
       StreamWriter DataSet;
@@ -95,31 +77,39 @@ namespace Acesso{
             }
             
               Auth.Close();
-          break;  
+
+          break;
+
           }
 
       Console.WriteLine("Gostaria de Doar ou Adotar um Pet? ");
       Console.WriteLine("Digite: \n 1 - DOAR \n 2 - ADOTAR");
       Cadastrar = int.Parse(Console.ReadLine());
+
       switch(Cadastrar){
+
         case 1:
 
             Doador = File.AppendText("doador.txt");
 
             Console.WriteLine("Digite a espécie? GATO ou CACHORRO: ");
             CadPets.Especie = Console.ReadLine().ToUpper();
+            contaDoacao.Add(CadPets.Especie);
             Doador.WriteLine(CadPets.Especie);
 
             Console.WriteLine("Qual a raça? ");
             CadPets.Raca = Console.ReadLine().ToUpper();
+            contaDoacao.Add(CadPets.Raca);
             Doador.WriteLine(CadPets.Raca);
 
             Console.WriteLine("Cidade do Doador");
             CadPets.Localidade = Console.ReadLine().ToUpper();
+            contaDoacao.Add(CadPets.Localidade);
             Doador.WriteLine(CadPets.Localidade);
 
             Console.WriteLine("Qual o porte? \n PEQUENO \n MEDIO \n GRANDE");
             CadPets.Porte = Console.ReadLine().ToUpper();
+            contaDoacao.Add(CadPets.Porte);
             Doador.WriteLine(CadPets.Porte);
 
             Console.WriteLine("Pet Cadastrado. Daremos ao PET o melhor cuidador! ");
@@ -130,41 +120,157 @@ namespace Acesso{
 
         case 2: 
 
-            Console.WriteLine( DBPets.ToString() );
-
             Adotante = File.AppendText("adotante.txt");
 
             Console.WriteLine("Prefere GATO ou CACHORRO? - Digite sua escolha");
             CadPets.Especie = Console.ReadLine().ToUpper();
+            contaAdocao.Add(CadPets.Especie);
             Adotante.WriteLine(CadPets.Especie);
 
             Console.WriteLine("Raça preferida: ");
             CadPets.Raca = Console.ReadLine().ToUpper();
             Adotante.WriteLine(CadPets.Raca);
+            contaAdocao.Add(CadPets.Raca);
 
             Console.WriteLine("Nome da Cidade que procura o Pet: ");
             CadPets.Localidade = Console.ReadLine().ToUpper();
             Adotante.WriteLine(CadPets.Localidade);
+            contaAdocao.Add(CadPets.Localidade);
 
             Console.WriteLine("Porte: PEQUENO - MEDIO - GRANDE ? ");
             CadPets.Porte = Console.ReadLine().ToUpper();
             Adotante.WriteLine(CadPets.Porte);
+            contaAdocao.Add(CadPets.Porte);
             
-            for(int i = 0; i < 1000; i++){
-                  if( DBPets[i,0] == CadPets.Especie &&
-                      DBPets[i,1] == CadPets.Raca &&
-                      DBPets[i,2] == CadPets.Localidade &&
-                      DBPets[i,3] == CadPets.Porte ){
-                  Console.WriteLine("Pet Encontrado");
-                }
-            }
             Adotante.Close();
           break;  
         }
+
+      if(CadUser.Telefone == "27999959595"){
+        int opcao;
+
+        Console.WriteLine("Deseja acessar o banco de dados? ");
+        Console.WriteLine("1 - SIM \n 2 - NÃO");
+
+        opcao = int.Parse(Console.ReadLine());
+      
+        switch(opcao){
+          case 1:
+
+            int filter;
+
+            Console.WriteLine("Abrir:  1 - LISTA DE ADOÇÃO \n 2 - DOAÇÃO ");
+            filter = int.Parse(Console.ReadLine());
+
+            if(filter == 1){
+              
+              int Count = 0;
+
+              foreach(var i in contaAdocao){
+                Console.WriteLine("Filtrar qtd por espécie: 1 - GATO 2 - CACHORRO");
+                CadPets.Especie = Console.ReadLine().ToUpper();
+                  if(i == CadPets.Especie){
+                    Count++;
+                  }
+
+                Console.WriteLine(Count + " encontrados");  
+                
+                Console.WriteLine("Filtrar qtd por Raça: ");
+                CadPets.Raca = Console.ReadLine().ToUpper();
+                  if(i == CadPets.Raca){
+                    Count++;
+                  }
+
+                 Console.WriteLine(Count + " encontrados");  
+
+                Console.WriteLine("Filtrar qtd por Cidade: ");
+                CadPets.Localidade = Console.ReadLine().ToUpper();
+                  if(i == CadPets.Localidade){
+                    Count++;
+                  }
+
+                Console.WriteLine(Count + " encontrados");  
+
+
+
+                Console.WriteLine("Filtrar qtd por Porte: ");
+                CadPets.Porte = Console.ReadLine().ToUpper();
+                  if(i == CadPets.Localidade){
+                    Count++;
+                  }
+
+                Console.WriteLine(Count + " encontrados");
+
+              }
+              
+
+            }
+          break;  
+
+            if(filter == 2){
+              int Count = 0;
+
+              foreach(var i in contaAdocao){
+                Console.WriteLine("Filtrar qtd por espécie: 1 - GATO 2 - CACHORRO");
+                CadPets.Especie = Console.ReadLine().ToUpper();
+                  if(i == CadPets.Especie){
+                    Count++;
+                  }
+
+                Console.WriteLine(Count + " encontrados");  
+                
+                Console.WriteLine("Filtrar qtd por Raça: ");
+                CadPets.Raca = Console.ReadLine().ToUpper();
+                  if(i == CadPets.Raca){
+                    Count++;
+                  }
+
+                 Console.WriteLine(Count + " encontrados");  
+
+                Console.WriteLine("Filtrar qtd por Cidade: ");
+                CadPets.Localidade = Console.ReadLine().ToUpper();
+                  if(i == CadPets.Localidade){
+                    Count++;
+                  }
+
+                Console.WriteLine(Count + " encontrados");  
+
+
+
+                Console.WriteLine("Filtrar qtd por Porte: ");
+                CadPets.Porte = Console.ReadLine().ToUpper();
+                  if(i == CadPets.Localidade){
+                    Count++;
+                  }
+
+                Console.WriteLine(Count + " encontrados");
+
+              }
+              
+
+            }
+          break; 
+
+
+          case 2:
+            Console.Clear();
+
+            Console.WriteLine("Obrigado por usar nosso sistema!");
+            Console.WriteLine("Até logo! ");
+            
+          break;
+
+            }
+
+          
+        }
+
+          
+      }
             
       }    
     }
-  }
+  
   
 
 
